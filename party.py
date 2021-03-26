da390a2cd632805fea3e43ac62d70e33d72ffb54
Yuliana <ya.aldrich@yahoo.com>
2021-03-26 19:53:18 +0000

    create test cases for is_mel and _most_and_least_common_type

--- party.py
+++ party.py
@@ -8,7 +8,14 @@ app.secret_key = "SECRETSECRETSECRET"
 
 
 def is_mel(name, email):
-    """Is this user Mel?"""
+    """Is this user Mel?
+        >>> is_mel('Judith Butler', 'judith@awesome.com')
+        False
+        >>> is_mel('Mel Melitpolski', 'mel@ubermelon.com')
+        True
+        
+    """
+       
 
     return name == "Mel Melitpolski" or email == "mel@ubermelon.com"
 
@@ -17,6 +24,17 @@ def most_and_least_common_type(treats):
     """Given list of treats, return most and least common treat types.
 
     Return most and least common treat types in tuple of format (most, least).
+
+    >>> test = []
+    >>> most_and_least_common_type(test)
+    (None, None)
+    >>> treats = [{'type': 'dessert'}, {'type': 'dessert'}, {'type': 'appetizer'}, {'type': 'dessert'}, {'type': 'appetizer'}, {'type': 'drink'}]
+    >>> most_and_least_common_type(treats)
+    ('dessert', 'drink')
+    >>> treats2 = [{'type': 'dessert'}, {'type': 'dessert'}, {'type': 'dessert'}, {'type': 'dessert'}, {'type': 'dessert'}, {'type': 'dessert'}]
+    >>> most_and_least_common_type(treats2)
+    ('dessert', 'dessert')
+
     """
 
     types = {}
